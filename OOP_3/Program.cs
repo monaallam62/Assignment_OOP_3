25e9c9548f02df3506d273e057097aaf9fc8636e
monaallam62 <monaallam5102@gmail.com>
2026-03-03 19:15:23 +0200

    Question 1

--- OOP_3/Program.cs
+++ OOP_3/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Question 1
+            //a) A University has Departments. If the university is closed, the departments no longer exist.
+            //Answer
+            //a)Composition relationship
+            //=======================================================
+            //b) A Driver uses a Car.The driver does not own the car.
+            //b)Association relationship
+            //=======================================================
+            //c) A Dog is an Animal.
+            //c)Inheritance
+            //========================================================
+            //d) A Team has Players. If the team is deleted, the players still exist.
+            //d)Aggregation
+            //========================================================
+            //e) A method receives a Logger as a parameter and calls it inside the method only
+            //e)Dependency
+            #endregion
+
         }
     }
 }
